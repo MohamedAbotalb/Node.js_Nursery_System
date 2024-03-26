950cf2652d135d076bb981d75ed635c9d9b8679a
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-03-27 01:05:27 +0200

    Add change password route end point

--- Routes/authentication.route.js
+++ Routes/authentication.route.js
@@ -5,6 +5,7 @@ const TeacherController = require('../Controllers/teacher.controller');
 const AuthValidator = require('../Middlewares/Validation/authentication.validator');
 const TeacherValidator = require('../Middlewares/Validation/teacher.validator');
 const validationResult = require('../Middlewares/Validation/ValidationResult');
+const upload = require('../Middlewares/file_upload.middleware');
 
 router.post(
   '/register',
@@ -12,12 +13,21 @@ router.post(
   validationResult,
   TeacherController.insert
 );
+
 router.post(
   '/login',
   AuthValidator.login(),
   validationResult,
   AuthController.login
 );
+
 router.get('/logout', AuthController.logout);
+
+router.patch(
+  '/changepassword',
+  AuthValidator.changePassword(),
+  validationResult,
+  AuthController.changePassword
+);
 
 module.exports = router;
