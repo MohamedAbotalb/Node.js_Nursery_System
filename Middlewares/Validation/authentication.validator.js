ca10e70fbfc8dc77ed1ea867ea96709f4b7a5c18
MohamedAbotalb <mohamed.abotalb277@gmail.com>
2024-03-27 01:06:40 +0200

    Add change password validation

--- Middlewares/Validation/authentication.validator.js
+++ Middlewares/Validation/authentication.validator.js
@@ -14,6 +14,25 @@ class AuthValidator {
         .withMessage('password should be more than 6 digits'),
     ];
   }
+
+  changePassword() {
+    return [
+      body('email')
+        .notEmpty()
+        .isEmail()
+        .withMessage('Email should be valid value'),
+      body('oldPassword')
+        .notEmpty()
+        .isStrongPassword()
+        .isLength({ min: 6 })
+        .withMessage('Old password should be more than 6 digits'),
+      body('newPassword')
+        .notEmpty()
+        .isStrongPassword()
+        .isLength({ min: 6 })
+        .withMessage('New password should be more than 6 digits'),
+    ];
+  }
 }
 
 module.exports = new AuthValidator();
